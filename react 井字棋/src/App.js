4fe05290ebc41dda884c854cc5f2a8834a11193d
luojiongfeng <2303273170@qq.com>
2026-01-19 22:23:10 +0800

    1-19学习内容

---
+++ react 井字棋/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(index) {
+    setSquares(squares.map((square, i) => i === index ? 'X' : square));
+  }
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
+}
+
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
